d0433824f67e515d6b459f69ce3f884f12f8eacf
Dmytro Rudenko <rud.rud.1999@gmail.com>
2019-09-03 19:19:23 +0300

    Create solution #1.cpp

---
+++ 901-1000/#0945 Minimum Increase To Make Array Unique/solution #1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& A) {
+        sort(A.begin(), A.end());
+        int ans = 0;
+        for (int i = 1; i < A.size(); i++) {
+            if (A[i] <= A[i - 1]) {
+                ans += A[i - 1] - A[i] + 1;
+                A[i] = A[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+};
